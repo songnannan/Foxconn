76be6f67aa59cec12e6be5f05a252a4fe9d820f3
unknown <unknown@example.com>
2020-04-15 17:00:28 +0800

    Initial Commit

---
+++ pages/location/location.js
@@ -0,0 +1,83 @@
+// pages/location/location.js
+//引用百度地圖微信小程序jsapi模塊
+// var bmap = require('../../libs/bmap-wx.js');
+var wxMarkerData = [];
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: { 
+    markers:[],
+    latitude:'',
+    longitude:'',
+    rgcData:{}
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  /**
+   * 獲取定位
+   */
+  // getLocation:function(){
+  //   type: '',
+  //   altitude: true,
+  //   success: function(res) {},
+  //   fail: function(res) {},
+  //   complete: function(res) {},
+  // }
+})
